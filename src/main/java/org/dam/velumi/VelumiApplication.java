b8e8ee3d755179e3dfafab3a4f97b518ab758232
Mahu2121 <hugogbmahu@gmail.com>
2026-08-19 17:27:56 +0200

    first commit

---
+++ src/main/java/org/dam/velumi/VelumiApplication.java
@@ -0,0 +1,13 @@
+package org.dam.velumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelumiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelumiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dam/velumi/VelumiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dam.velumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelumiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
